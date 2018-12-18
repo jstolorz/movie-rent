691725b22235aebf0d1d26c2542da9b438681829
jstolorz <Jmnz1234>
2018-12-18 16:06:50 +0100

    Adding Customer route and repo.

--- db/repoCustomer.js
+++ db/repoCustomer.js
@@ -31,10 +31,28 @@ module.exports.persist = async function persist(customer){
 };
 
 module.exports.getAll = async function getAll(){
-    return cust = await Customer.find()
+    return await Customer.find()
         .sort({name: 1})
         .select({isGold: 1, name: 1, phone: 1});
 };
 
 
+module.exports.findOne = async function findOne(id){
+    console.log(id);
+    return await Customer.findOne({ _id: id});
+};
+
+module.exports.update = async function update(id,customer) {
+    console.log('id: ',id);
+    return await Customer.findOneAndUpdate({_id: id}, {
+        isGold: customer.isGold,
+        name: customer.name,
+        phone: customer.phone
+    }, {new: true});
+};
+
+
+module.exports.remove = async function remove(id) {
+    return await Customer.findOneAndDelete({_id: id});
+};
 
--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../db/repoCustomer');
+const Joi = require('joi');
 
 const router = express.Router();
 
@@ -11,6 +12,11 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req,res) => {
+
+    const {error} = validationGeneres(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+
 
     const customer = {
         isGold: req.body.isGold,
@@ -24,5 +30,52 @@ router.post('/', async (req,res) => {
 
 });
 
+router.get('/:id',async (req, res) => {
+
+    const customer = await db.findOne(req.params.id);
+
+    if(!customer){
+        res.status(404).send('The genre with given ID was not found.');
+    }else{
+        res.send(customer);
+    }
+});
+
+router.put('/:id',async (req, res) => {
+
+    const {error} = validationGeneres(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const customer = await db.update(req.params.id, req.body);
+
+    if(!customer){
+        res.status(404).send('The genre with given ID was not found.');
+    }else{
+        res.send(customer);
+    }
+
+});
+
+router.delete('/:id',async (req, res) => {
+    const customer = await db.remove(req.params.id);
+
+    if(!customer){
+        res.status(404).send('The genre with given ID was not found.');
+    }else{
+
+        res.send(customer);
+    }
+
+});
+
+function validationGeneres(customer){
+    const schema = {
+        isGold: Joi.boolean().required(),
+        name: Joi.string().required(),
+        phone: Joi.string().min(9).required()
+    };
+    return Joi.validate(customer, schema);
+}
 
 module.exports = router;
